7a20126c899e03278ce42abe162a004dc74e303b
phizzl3 <phizzl3@gmail.com>
2021-04-28 20:20:41 -0500

    Update re_file_search.py Reformatted to include a main function that can return a list with 1 or multiple file paths and 2 smaller functions that call the main function to return either a single item (first found) or a list of all matching items.

--- re_file_search.py
+++ re_file_search.py
@@ -1,7 +1,7 @@
 """
-Recursively searches the passed directory (searchdir) for a file that 
-matches the passed regular expression (regex) and returns it's location 
-as a Path object if found. (Return None if not.)
+Recursively searches the passed directory (searchdir) for files matching
+the passed regular expression (regex) and returns either the first match
+or a list of matches formatted as a Path objects if found.
 """
 
 import os
@@ -11,9 +11,9 @@ from pathlib import Path
 
 def get_file(searchdir: str, regex: str) -> Path:
     """
-    Recursively searches the passed directory (searchdir) for the first 
-    file that matches the passed regular expression (regex) and returns 
-    it's location as a Path object if found. (Returns None if not found.)
+    Recursively searches the passed directory (searchdir) for files matching
+    the passed regular expression (regex) and returns the first match found
+    formatted as a pathlib.Path object. 
 
     Args:
         searchdir (str/pathlib.Path): Directory to search for file.
@@ -21,12 +21,71 @@ def get_file(searchdir: str, regex: str) -> Path:
         for a match.
 
     Returns:
-        Path: Path object pointing to to found file. (Returns None if file
-        not found.)
+        pathlib.Path: File path of first file matching the searched regex. 
     """
+    return _find_matches(searchdir, regex, first=True)[0]
+
+
+def get_list(searchdir: str, regex: str) -> list:
+    """
+    Recursively searches the passed directory (searchdir) for files matching
+    the passed regular expression (regex) and returns a list of matches
+    formatted as pathlib.Path objects if found. 
+
+    Args:
+        searchdir (str/pathlib.Path): Directory to search for file.
+        regex (str): Regular expression pattern to search filenames 
+        for a match.
+
+    Returns:
+        list: List of file paths (pathlib.Path objects) that includes all 
+        matching filenames. 
+    """
+    return _find_matches(searchdir, regex)
+
+
+def _find_matches(searchdir: str, regex: str, first: bool = False) -> list:
+    """
+    This function is called by the other included functions to generate 
+    their output.
+
+    Recursively searches the passed directory (searchdir) for files matching
+    the passed regular expression (regex) and returns a list of matches
+    formatted as pathlib.Path objects if found. Returns a list with a single 
+    match (first one found) if first=True.
+
+    Args:
+        searchdir (str/pathlib.Path): Directory to search for file.
+        regex (str): Regular expression pattern to search filenames 
+        for a match.
+        first (bool, optional): T/F flag that determines if a list 
+        with only the first item found is returned. Defaults to False.
+
+    Returns:
+        list: List of file paths (pathlib.Path objects) that includes all 
+        matching filenames. Only includes a single item if first=True.
+    """
+    paths_list = []
     for root, _dirs, files in os.walk(searchdir):
         for file in files:
             if re.search(regex, file):
-                return Path(f'{root}/{file}')
-    else:
-        return None
+                paths_list.append(Path(f'{root}/{file}'))
+            if paths_list and first:
+                return paths_list
+    return paths_list
+
+
+if __name__ == '__main__':
+    # Test run...
+    p = Path(__file__).parent
+    sp = r'.+\..[md|py]$'
+
+    # First file found:
+    f = get_file(p, sp)
+    print(f"\nFirst found:\n{f}")
+
+    # List of matching files:
+    l = get_list(p, sp)
+    print("\nList:")
+    for each in l:
+        print(each)
