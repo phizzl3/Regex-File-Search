8a906203119cbc203aa1f84781e82e675d0977bb
Brandon Phillips <phizzl3@gmail.com>
2023-12-20 09:08:43 -0600

    Rename main functions and parameter (#3)

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# reFileSearch
+# RE-File-Search
 
 A quick, reusable file search module using a regular expression to search
 a file path for a filename.  
 
-Recursively searches the passed directory (searchdir) for files matching
+Recursively searches the passed directory (search_dir) for files matching
 the passed regular expression (regex) and returns either the first match
 or a list of matches formatted as a Path objects if found.  
 
@@ -15,8 +15,8 @@ Drop it into your working folder and import to use.
 import re_file_search
 
 # Get a single file path:
-needed_file = re_file_search.get_file('/Users/dev/Downloads/', 'Invoice.*pdf$')
+needed_file = re_file_search.re_get_file('/Users/dev/Downloads/', 'Invoice.*pdf$')
 
 # Get a list of file paths:
-files_list = re_file_search.get_list('/Users/dev/Downloads/', 'Invoice.*pdf$') 
+files_list = re_file_search.re_get_list('/Users/dev/Downloads/', 'Invoice.*pdf$') 
 ```
--- re_file_search.py
+++ re_file_search.py
@@ -1,7 +1,11 @@
 """
-Recursively searches the passed directory (searchdir) for files matching
+
+Recursively searches the passed directory (search_dir) for files matching
 the passed regular expression (regex) and returns either the first match
 or a list of matches formatted as a Path objects if found.
+
+Rev: 12/20/2023
+
 """
 
 import os
@@ -9,14 +13,13 @@ import re
 from pathlib import Path
 
 
-def get_file(searchdir: str, regex: str) -> Path:
-    """
-    Recursively searches the passed directory (searchdir) for files matching
+def re_get_file(search_dir: str, regex: str) -> Path:
+    """Recursively searches the passed directory (search_dir) for files matching
     the passed regular expression (regex) and returns the first match found
     formatted as a pathlib.Path object. 
 
     Args:
-        searchdir (str/pathlib.Path): Directory to search for file.
+        search_dir (str/pathlib.Path): Directory to search for file.
         regex (str): Regular expression pattern to search filenames 
         for a match.
 
@@ -24,19 +27,18 @@ def get_file(searchdir: str, regex: str) -> Path:
         pathlib.Path: File path of first file matching the searched regex. 
     """
     try:
-        return _find_matches(searchdir, regex, first=True)[0]
+        return _find_matches(search_dir, regex, first=True)[0]
     except IndexError:
         return None
 
 
-def get_list(searchdir: str, regex: str) -> list:
-    """
-    Recursively searches the passed directory (searchdir) for files matching
+def re_get_list(search_dir: str, regex: str) -> list:
+    """Recursively searches the passed directory (search_dir) for files matching
     the passed regular expression (regex) and returns a list of matches
     formatted as pathlib.Path objects if found. 
 
     Args:
-        searchdir (str/pathlib.Path): Directory to search for file.
+        search_dir (str/pathlib.Path): Directory to search for file.
         regex (str): Regular expression pattern to search filenames 
         for a match.
 
@@ -44,21 +46,21 @@ def get_list(searchdir: str, regex: str) -> list:
         list: List of file paths (pathlib.Path objects) that includes all 
         matching filenames. 
     """
-    return _find_matches(searchdir, regex)
+    return _find_matches(search_dir, regex)
 
 
-def _find_matches(searchdir: str, regex: str, first: bool = False) -> list:
+def _find_matches(search_dir: str, regex: str, first: bool = False) -> list:
     """
     This function is called by the other included functions to generate 
     their output.
 
-    Recursively searches the passed directory (searchdir) for files matching
+    Recursively searches the passed directory (search_dir) for files matching
     the passed regular expression (regex) and returns a list of matches
     formatted as pathlib.Path objects if found. Returns a list with a single 
     match (first one found) if first=True.
 
     Args:
-        searchdir (str/pathlib.Path): Directory to search for file.
+        search_dir (str/pathlib.Path): Directory to search for file.
         regex (str): Regular expression pattern to search filenames 
         for a match.
         first (bool, optional): T/F flag that determines if a list 
@@ -69,7 +71,7 @@ def _find_matches(searchdir: str, regex: str, first: bool = False) -> list:
         matching filenames. Only includes a single item if first=True.
     """
     paths_list = []
-    for root, _dirs, files in os.walk(searchdir):
+    for root, _dirs, files in os.walk(search_dir):
         for file in files:
             if re.search(regex, file):
                 paths_list.append(Path(f'{root}/{file}'))
@@ -84,11 +86,11 @@ if __name__ == '__main__':
     sp = r'.+\..[md|py]$'
 
     # First file found:
-    f = get_file(p, sp)
+    f = re_get_file(p, sp)
     print(f"\nFirst found:\n{f}")
 
     # List of matching files:
-    l = get_list(p, sp)
+    l = re_get_list(p, sp)
     print("\nList:")
     for each in l:
         print(each)
